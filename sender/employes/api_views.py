1e2f41c0041c214f9d5937a5a7dbbdff93752854
Simanto <simanto.saha2167@gmail.com>
2026-01-02 22:32:46 +0600

    all

--- sender/employes/api_views.py
+++ sender/employes/api_views.py
@@ -1,10 +1,9 @@
-from django.shortcuts import render
 from .models import Employes
 from .serializers import EmployesSerializer
 from rest_framework.response import Response
 from rest_framework import status
-from rest_framework.decorators import api_view
 from rest_framework.views import APIView
+from django.http import Http404
 
 
 
@@ -25,4 +24,32 @@ class Employes_list(APIView):
         else:
             return Response(serialzer.errors, status=status.HTTP_400_BAD_REQUEST)
         
+class Employes_details(APIView):
+
+    def get_object(self, pk):
+        try:
+            return Employes.objects.get(pk=pk)
+        except Employes.DoesNotExist:
+            raise Http404
         
+    def get(self, request, pk):
+
+        employe = self.get_object(pk)
+        serializer = EmployesSerializer(employe)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    
+    def put(self, request, pk):
+
+        employe = self.get_object(pk)
+        serializer = EmployesSerializer(employe, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    
+    def delete(self, request, pk):
+
+        employe = self.get_object(pk)
+        employe.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
